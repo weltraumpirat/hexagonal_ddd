65cd8088d99c6c7ab77d0c8224bf613247b7c8d4
tobias.goeschel <tobias.goeschel@codecentric.de>
2020-07-29 00:47:30 +0200

    Added explicit build step to docker-compose to be able to drop manually removing the images for rebuild.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-docker rmi -f example-shop shop-gui admin-gui shop-lb
 cp example-shop/target/example-shop-*.jar docker/
 cp -R shop-gui/build/ docker/shop
 cp -R admin-gui/build/ docker/admin
-docker-compose --file docker/docker-compose.yml up --force-recreate
+docker-compose --file docker/docker-compose.yml build
+docker-compose --file docker/docker-compose.yml up
