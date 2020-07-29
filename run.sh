0c3927eec7a0904b5af79f993055973e09746fd0
tobias.goeschel <tobias.goeschel@codecentric.de>
2020-07-30 00:35:21 +0200

    Add two new modules (product and shopping), and set up deployment.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
 cp example-shop-order/target/example-shop-order-*.jar docker/
+cp example-shop-product/target/example-shop-product-*.jar docker/
+cp example-shop-shoppingcart/target/example-shop-shoppingcart-*.jar docker/
 cp -R shop-gui/build/ docker/shop
 cp -R admin-gui/build/ docker/admin
 docker-compose --file docker/docker-compose.yml build
